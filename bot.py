921287b9153dcb3ba4f627fb1fd39f055ba008c3
gourav <gouravsarath7@gmail.com>
2024-10-31 15:18:09 +0530

    Initial commit of the bot

---
+++ bot.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+
+# Define intents (permissions) for the bot
+intents = discord.Intents.default()
+intents.members = True  # This enables member-related events, like joining
+
+# Set up the bot with prefix '!' and intents
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+@bot.event
+async def on_member_join(member):
+    # Find the "general" channel or replace with your welcome channel name
+    channel = discord.utils.get(member.guild.text_channels, name="general")
+    if channel:
+        await channel.send(f'Eda Mownea.....Welcome to Mallu Programmers....., {member.mention}!')
+
+# Run the bot with your token
+bot.run('MTMwMTQ3MDY0NDcxMzgxNjA4Ng.G0MMy1.J899qpN4LDmneUnV6sUEgYJ-SmWCke5bAqSckc')
